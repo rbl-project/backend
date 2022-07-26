ae8cb366d819357f409aa8ee5d0f96614800776c
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2022-07-27 01:31:54 +0530

    Changed schema to test migrations

--- models/user_model.py
+++ models/user_model.py
@@ -7,6 +7,7 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
+    phone = db.Column(db.String())
     db_count = db.Column(db.Integer())
     date_added = db.Column(db.DateTime, default=datetime.datetime.now())
     password_hash = db.Column(db.String(128))
