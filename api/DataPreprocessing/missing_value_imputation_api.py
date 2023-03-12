e1332f198e7141d8c9dcc8cf5a4fe05ac5983690
Shreyas Jadhav <jshreyas12@gmail.com>
2023-03-12 21:07:03 +0530

    Get Missing Value Percentage API Working

--- api/DataPreprocessing/missing_value_imputation_api.py
+++ api/DataPreprocessing/missing_value_imputation_api.py
@@ -70,41 +70,39 @@ def get_missing_value_percentage():
             
         # ================================== Main Logic Start HERE ==================================
     
-        # # Get the percentage of missing values in each column
-        # cols = df.columns.tolist()
-        # print(type(column_wise_missing_value_type))
-        # column_wise_missing_value_data = []
-        # for col in cols:
-        #     print(column_wise_missing_value_type[col])
-        #     missing_percentage = round(df[col].eq(column_wise_missing_value_type[col]["missing_value"]).sum()/len(df[col]) * 100, 2)
-        #     non_missing_percentage = 100 - missing_percentage
-        #     column_wise_missing_value_data.append({
-        #         "column_name": col, 
-        #         "missing_value_percentage": missing_percentage, 
-        #         "correct_value_percentage": non_missing_percentage,
-        #         "missing_value_type": column_wise_missing_value_type[col]["missing_value"]
-        #     })
+        # Get the percentage of missing values in each column
+        cols = df.columns.tolist()
+        column_wise_missing_value_data = []
+        for col in cols:
+            missing_percentage = round(df[col].eq(column_wise_missing_value_type[col]["missing_value"]).sum()/len(df[col]) * 100, 2)
+            non_missing_percentage = 100 - missing_percentage
+            column_wise_missing_value_data.append({
+                "column_name": col, 
+                "missing_value_percentage": missing_percentage, 
+                "correct_value_percentage": non_missing_percentage,
+                "missing_value_type": column_wise_missing_value_type[col]["missing_value"]
+            })
         
-        # # Sort the column wise missing value data in descending order of missing value percentage
-        # column_wise_missing_value_data.sort(key=lambda x: x["missing_value_percentage"], reverse=True)
+        # Sort the column wise missing value data in descending order of missing value percentage
+        column_wise_missing_value_data.sort(key=lambda x: x["missing_value_percentage"], reverse=True)
         
-        # # Get the total percentage of missing values in the dataset
-        # total_missing_value_percentage = round(df.eq(all_columns_missing_value_type["missing_value"]).sum().sum()/df.size * 100, 2)
-        # total_non_missing_value_percentage = 100 - total_missing_value_percentage
-        # total_missing_value_data = {
-        #     "column_name": "all_columns",
-        #     "missing_value_percentage": total_missing_value_percentage,
-        #     "correct_value_percentage": total_non_missing_value_percentage,
-        #     "missing_value_type": all_columns_missing_value_type["missing_value"]
-        # }
+        # Get the total percentage of missing values in the dataset
+        total_missing_value_percentage = round(df.eq(all_columns_missing_value_type["missing_value"]).sum().sum()/df.size * 100, 2)
+        total_non_missing_value_percentage = 100 - total_missing_value_percentage
+        total_missing_value_data = {
+            "column_name": "all_columns",
+            "missing_value_percentage": total_missing_value_percentage,
+            "correct_value_percentage": total_non_missing_value_percentage,
+            "missing_value_type": all_columns_missing_value_type["missing_value"]
+        }
         
-        # # ================================ Main Logic Ends HERE =================================
+        # ================================ Main Logic Ends HERE =================================
         
-        # res = {
-        #     "column_wise_missing_value_data": column_wise_missing_value_data,
-        #     "total_missing_value_data": total_missing_value_data,
-        # }   
-        res = "Hello"
+        res = {
+            "column_wise_missing_value_data": column_wise_missing_value_data,
+            "total_missing_value_data": total_missing_value_data,
+        }   
+        
         return respond(data=res)
     
     except Exception as e:
