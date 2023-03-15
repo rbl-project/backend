5a855fcd9578e69f556acfe44cd4144462891ece
Shreyas Jadhav <jshreyas12@gmail.com>
2023-03-15 11:51:07 +0530

    MongoDB Setup Complete

--- api/DataPreprocessing/missing_value_imputation_api.py
+++ api/DataPreprocessing/missing_value_imputation_api.py
@@ -11,11 +11,12 @@ from utilities.methods import load_dataset_copy, load_dataset, log_error, make_d
 
 # MODELS
 from models.user_model import Users
-from models.dataset_metadata_model import Man
+from models.dataset_metadata_model import MetaData
 # constants
 
 # OTHER
 import json
+from datetime import datetime as dt
 
 # BLUEPRINT
 missingValueImputationAPI = Blueprint("missingValueImputationAPI", __name__)
@@ -27,7 +28,29 @@ missingValueImputationAPI_restful = Api(missingValueImputationAPI)
 @jwt_required()
 def create():
     print("create")
-    new_man = Man(name="Prashant", email="prashant@123")
-    new_man.save()
-    return respond(data={"message": "Created","data":new_man})
+    new_metadata = MetaData(
+        id = "id",
+        user_id = "user_id",
+        user_email = "user_email",
+        is_copy = False,
+        date_created = dt.now(),
+        last_modified = dt.now(),
+        dataset_name = "dataset_name",
+        dataset_extension = "dataset_extension",
+        dataset_file_name = "dataset_file_name",
+        dataset_size = 100.5,
+        n_rows = 100,
+        n_columns = 100,
+        n_values = 100,
+        column_list = ["colum1","colum2"],
+        column_datatypes = {"colum1":"int","colum2":"string"},
+        numerical_column_list = ["colum1"],
+        categorical_column_list = ["colum2"],
+        column_wise_missing_value = {"colum1":0,"colum2":0},
+        all_columns_missing_value = {"missing_value":0}
+    )
+    
+    new_metadata.save()
+    
+    return respond(data={"message": "Created","data":new_metadata})
 
