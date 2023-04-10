10ff64f9a7696fd0e85c5bef9609f8e0cb091675
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2023-04-10 20:09:47 +0530

    Completed the feature encoding project

--- api/FeatureEngineering/feature_encoding_api.py
+++ api/FeatureEngineering/feature_encoding_api.py
@@ -418,6 +418,118 @@ def frequency_encoding():
         return respond(error=err)
 
 
+# Api for ordinal  encoding
+@featureEncodingAPI.route("/ordinal-encoding", methods=["POST"])
+@jwt_required()
+def ordinal_encoding():
+    """
+        TAKES dataset_name, column list and mapping status as input
+        PERFORMS ordinal encoding on the dataset
+        RETURNS success as response
+    """
+    err = None
+    try:
+        current_user = get_jwt_identity()
+        user = Users.query.filter_by(id=current_user["id"]).first()
+        if not user:
+            err = "No such user exits"
+            raise
+
+        if not request.is_json:
+            err="Missing JSON in request"
+            raise
+        
+        dataset_name = request.json.get("dataset_name")
+        if not dataset_name:
+            err = "Dataset name is required"
+            raise
+
+        """
+            Request Body {
+                "dataset_name": "dataset_name",
+                "encoding_info": {
+                    "column_name": "col_name",
+                    "custom_mapping": True,
+                    "mapping": {
+                        "cat_1": 1,
+                        "cat_2": 2
+                    }
+                }
+            }
+        """
+        encoding_info = request.json.get("encoding_info")
+        if not encoding_info:
+            err = "Encoding info is required"
+            raise
+        if not isinstance(encoding_info, dict):
+            err = "Encoding info should be a dictionary"
+            raise
+        
+        column_name = encoding_info.get("column_name")
+        if not column_name:
+            err = "Column name is required"
+            raise
+
+        custom_mapping = encoding_info.get("custom_mapping", False)
+        if not isinstance(custom_mapping, bool):
+            err = "Custom mapping status should be a boolean"
+            raise
+
+        mapping = encoding_info.get("mapping")
+        if custom_mapping and not mapping:
+            err = "Mapping information is required for custom mapping"
+            raise
+        if custom_mapping and not isinstance(mapping, dict):
+            err = "Mapping should be a dictionary"
+            raise
+
+         # Look if the copy of dataset exists and if it does, then rename the columns in that copy otherwise rename make a copy and rename the columns in that copy
+        if not check_dataset_copy_exists(dataset_name, user.id, user.email):
+            app.logger.info("Dataset copy of %s does not exist. Trying to make a copy of the dataset", dataset_name)
+            err = make_dataset_copy(dataset_name, user.id, user.email)
+            if err:
+                raise
+    
+        df, err = load_dataset_copy(dataset_name, user.id, user.email)
+        if err:
+            raise
+
+        # ===================== Business Logic Starts Here =====================
+
+        # Check if the column name is valid
+        if column_name not in df.columns:
+            err = f"Column '{column_name}' does not exists in the dataset"
+            raise
+
+        if custom_mapping:
+            custom_mapping_obj = [
+                {
+                    'col': column_name, 
+                    'mapping': mapping
+                }
+            ]
+            df = OrdinalEncoder(mapping=custom_mapping_obj, return_df=True).fit(df).transform(df)
+        
+        else: 
+            from sklearn.preprocessing import OrdinalEncoder as sklearn_OrdinalEncoder
+            df[column_name] = sklearn_OrdinalEncoder().fit(df[[column_name]]).transform(df[[column_name]])
+        
+        # ===================== Business Logic Ends Here =====================
+
+        # Save the dataset copy and update the metadata
+        update_metadata(user, dataset_name, df)
+
+        res = {
+            "msg": "Ordinal encoding successful"
+        }
+        return respond(data=res)
+    
+    except Exception as e:
+        log_error(err_msg="Error in ordinal encoding", error=err, exception=e)
+        if not err:
+            err = "Error in ordinal encoding"
+        return respond(error=err)
+
 def update_metadata(user, dataset_name, df):
     try:
         # get new column data
